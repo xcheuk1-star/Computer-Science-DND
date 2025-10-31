2a59eae17f6422bf7a450342b906edf7f9673361
xcheuk1-star <xcheuk1@hwemail.com>
2025-10-31 09:16:03 -0700

    worked on recursion

--- Recursion/Tester.java
+++ Recursion/Tester.java
@@ -1,11 +1,15 @@
 public class Tester {
     public static void main(String[] args) {
-        ListNode one = new ListNode("One");
-        ListNode two = new ListNode("Two");
-        ListNode three = new ListNode("Three");
-        one.setNext(two);
-        two.setNext(three);
+        // ListNode one = new ListNode("One");
+        // ListNode two = new ListNode("Two");
+        // ListNode three = new ListNode("Three");
+        // one.setNext(two);
+        // two.setNext(three);
 
-        Recursion.printListInReverse(one);
+        // Recursion.printListInReverse(one);
+
+        System.out.println(Recursion.countNonConsecutiveSubsets(5));
+        System.out.println(Recursion.countWaysToJumpUpStairs(5));
+        Recursion.printSubsets("abc");
     }
 }
